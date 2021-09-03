80784aeb51b1675285c3cb33c9fd144be9688944
unext1 <unextbusinessman@gmail.com>
2021-09-03 10:40:45 +0200

    testing mobile bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import Navbar from "../components/navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="">
+      <Navbar />
       <Component {...pageProps} />
     </div>
   );
